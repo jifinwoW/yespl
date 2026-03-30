f4d057aea136892fc484cd1ba5de9813abd4b66f
OTHER_USERNAME <remiz@tottlabs.com>
2026-03-30 10:30:55 +0530

    Implement AMC state field in HelpDesk module with picklist values

--- mig2.php
+++ mig2.php
@@ -6,28 +6,30 @@ include_once('vtlib/Vtiger/Module.php');
 // ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
 
-
-$moduleInstance = Vtiger_Module::getInstance('Tickets');
-$blockInstance = Vtiger_Block::getInstance('LBL_TICKETS_INFORMATION', $moduleInstance);
-
-$fieldInstance = Vtiger_Field::getInstance('amc_id', $moduleInstance);
-
-if (!$fieldInstance && $blockInstance) {
+$moduleInstance = null;
+$blockInstance = null;
+$fieldInstance = null;
+$moduleInstance = Vtiger_Module::getInstance('HelpDesk');
+$blockInstance = Vtiger_Block::getInstance('Address Details', $moduleInstance);
+if ($blockInstance) {
+    $fieldInstance = Vtiger_Field::getInstance('amc_state', $moduleInstance);
+    if (!$fieldInstance) {
         $fieldInstance = new Vtiger_Field();
-    $fieldInstance->name = "amc_id";
-    $fieldInstance->label = "AMC";
-    $fieldInstance->table = $moduleInstance->basetable;
-    $fieldInstance->column = "amc_id";
-    $fieldInstance->columntype = 'VARCHAR(200)';
-    $fieldInstance->uitype = 10;
-    $fieldInstance->displaytype = 1;
-    $fieldInstance->presence = 2;
-    $fieldInstance->quickcreate = 0;
-    $fieldInstance->typeofdata = 'V~O';
-    $blockInstance->addField($fieldInstance);
-    $fieldInstance->setRelatedModules(['HelpDesk']);
-    echo "Field created.";
+        $fieldInstance->name = 'amc_state';
+        $fieldInstance->label = 'State';
+        $fieldInstance->table = $moduleInstance->basetable;
+        $fieldInstance->column = 'amc_state';
+        $fieldInstance->uitype = '16';
+        $fieldInstance->presence = '0';
+        $fieldInstance->typeofdata = 'V~O';
+        $fieldInstance->columntype = 'VARCHAR(100)';
+        $fieldInstance->defaultvalue = NULL;
+        $blockInstance->addField($fieldInstance);
+        $fieldInstance->setPicklistValues(array('Assam', 'Bihar', 'Chhattisgarh', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu & Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Mizoram', 'New Delhi', 'Odisha', 'Punjab', 'Rajasthan', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'));
+    } else {
+        echo "field is already Present --- amc_state in HelpDesk Module --- <br>";
+    }
 } else {
-    echo "Field already exists or block not found.";
+    echo " block does not exits --- Address Details -- <br>";
 }
 
