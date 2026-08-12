05eaa82066f5dab2cdc1b779d7d5bface0a655ec
Jifin KJ <jifinjosy007@gmail.com>
2026-08-12 05:14:14 +0000

    feat: add language strings for Ticket Time Log functionality

---
+++ languages/en_us/TicketTimeLog.php
@@ -0,0 +1,17 @@
+<?php
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ *************************************************************************************/
+$languageStrings = array(
+    'LBL_ADD_RECORD' => 'Add Ticket Time Log',
+    'SINGLE_TicketTimeLog' => 'Ticket Time Log',
+    'LBL_TTL_DETAILS' => 'Ticket Time Log Details',
+    'TicketTimeLog' => 'Ticket Time Log',
+    'Products List' => 'Spare Part List',
+    'Product' => 'Spare Part'
+);
